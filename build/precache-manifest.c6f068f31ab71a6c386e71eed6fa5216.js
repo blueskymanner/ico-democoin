c01c03f119efebf3e0c1e12e17439f293ba4587f
bluskymanner <page111first@gmail.com>
2022-04-10 02:21:27 +0200

    added build

---
+++ build/precache-manifest.c6f068f31ab71a6c386e71eed6fa5216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aae87c8eeadaecf5259b",
+    "url": "/static/css/main.f297f893.chunk.css"
+  },
+  {
+    "revision": "aae87c8eeadaecf5259b",
+    "url": "/static/js/main.aae87c8e.chunk.js"
+  },
+  {
+    "revision": "14c2761410b70ec861b8",
+    "url": "/static/js/1.14c27614.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80866bfd9790ed8ffe19e408c21f0772",
+    "url": "/index.html"
+  }
+];
